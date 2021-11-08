355e595f40ebf96aab39621cd2d4627fc3a1b416
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-11-08 15:52:55 -0600

    Added username to user table in database.sql

--- database.sql
+++ database.sql
@@ -8,10 +8,11 @@
 
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (255) UNIQUE NOT NULL,
+    "password" VARCHAR (255) NOT NULL,
     "first_name" VARCHAR (255),
     "last_name" VARCHAR (255),
-    "email" VARCHAR (255),
-    "password" VARCHAR (255)
+    "email" VARCHAR (255)
 );
 
 CREATE TABLE "format" (
