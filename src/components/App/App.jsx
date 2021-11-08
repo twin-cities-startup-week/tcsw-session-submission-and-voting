d86aa496a2ee69d0ecf723e28cd7c2d070daaa56
Sarah Helgen <sarah.helgen@gmail.com>
2021-11-08 16:34:52 -0600

    working on form, adding some styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -73,7 +73,7 @@ function App() {
             exact
             path="/submission"
           >
-            <SubmissionForm />
+            <SubmissionPage />
           </Route>
 
           <Route
--- src/components/SubmissionPage/SubmissionPage.jsx
+++ src/components/SubmissionPage/SubmissionPage.jsx
@@ -1,10 +1,34 @@
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container'
 
-function SubmissionPage(){
+
+function SubmissionPage() {
 
 
-    return(
+    return (
+        <>
+        <Box p={2}>
+            <Typography variant="h5" align="left">
+                TCSW Session Submission 2022
+            </Typography>
+            </Box>
 
-        <h3>TCSW Submission Form Here!</h3>
+            <Box p={3}>
+            <Container component={Paper}>
+            <FormControl>
+            <Box p={2}>
+            <Typography variant="body2" gutterBottom> Email Address:</Typography>
+            <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth />
+            </Box>
+
+            </FormControl>
+            </Container>
+            </Box>
+        </>
     )
 }
 
