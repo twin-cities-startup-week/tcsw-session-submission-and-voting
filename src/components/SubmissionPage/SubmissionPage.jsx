0d1393b49281ab3607a558863bab2787f5ca29c7
Sarah Helgen <sarah.helgen@gmail.com>
2021-11-08 16:44:38 -0600

    email, phone, host, title inputs complete

--- src/components/SubmissionPage/SubmissionPage.jsx
+++ src/components/SubmissionPage/SubmissionPage.jsx
@@ -20,10 +20,25 @@ function SubmissionPage() {
             <Box p={3}>
             <Container component={Paper}>
             <FormControl>
-            <Box p={2}>
+            <Box p={1}>
             <Typography variant="body2" gutterBottom> Email Address:</Typography>
-            <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth />
+            <Typography variant="caption" display="block" gutterBottom>This is the email we will use for all TCSW-related communications.</Typography>
+            <TextField fullWidth id="outlined-basic" label="Email" variant="outlined"  />
             </Box>
+            <Box p={1}>
+            <Typography variant="body2" gutterBottom> Phone:</Typography>
+            <Typography variant="caption" display="block" gutterBottom>This is the phone number we will use for all TCSW-related communications.</Typography>
+            <TextField fullWidth id="outlined-basic" label="Phone" variant="outlined" />
+            </Box>
+            <Box p={1}>
+            <Typography variant="body2" gutterBottom>Is this event being hosted by an organization, company or other entity? If so, list here. If not, tell us a little bit about yourself.</Typography>
+            <TextField fullWidth id="outlined-basic" label="Host" variant="outlined" />
+            </Box>
+            <Box p={1}>
+            <Typography variant="body2" gutterBottom>Title of your event: </Typography>
+            <TextField fullWidth id="outlined-basic" label="Title" variant="outlined" />
+            </Box>
+
 
             </FormControl>
             </Container>
