fd99a2e6c3a64ec0d203571699632a4700e1ee05
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-11-09 15:58:00 -0600

    added an AboutPage that holds info and learn button

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -39,6 +39,10 @@ function Nav() {
         <Link className="navLink" to="/about">
           About
         </Link>
+
+        <Link className="navLink" to="/faq">
+          Faq
+        </Link>
       </div>
     </div>
   );
