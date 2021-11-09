d4f3e54798fe28dbd2fc2d18cfe951ff8720b256
Sarah Helgen <sarah.helgen@gmail.com>
2021-11-09 15:47:06 -0600

    adding local state to submission form

--- src/components/SubmissionPage/SubmissionPage.jsx
+++ src/components/SubmissionPage/SubmissionPage.jsx
@@ -144,14 +144,30 @@ function SubmissionPage() {
         console.log('adding a new submission');
         const newSubmission ={
             email: email,
+            phone: phone,
+            host: host,
+            title: title,
+            description: description,
+            attendees: attendees,
+            location: location,
+
+
+
+
 
         }
         console.log('The new submission is', newSubmission );
     }
 
     //variables for individual form questions
-
     const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [host, setHost] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription ] = useState('');
+    const [attendees, setAttendees ] = useState('');
+    const [location, setLocation ] = useState('');
+    const [locationDetails, setLocationDetails ] = useState('');
 
     return (
         <>
@@ -171,38 +187,38 @@ function SubmissionPage() {
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom> Phone:</Typography>
                             <Typography variant="caption" display="block" gutterBottom>This is the phone number we will use for all TCSW-related communications.</Typography>
-                            <TextField fullWidth id="outlined-basic" label="Phone" variant="outlined" />
+                            <TextField fullWidth id="outlined-basic" label="Phone" variant="outlined" required value={phone} onChange={(event) => setPhone(event.target.value)}/>
                         </Box>
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom>Is this event being hosted by an organization, company or other entity? If so, list here. If not, tell us a little bit about yourself.</Typography>
-                            <TextField fullWidth id="outlined-basic" label="Host" variant="outlined" />
+                            <TextField fullWidth id="outlined-basic" label="Host" variant="outlined" required value={host} onChange={(event) => setHost(event.target.value)} />
                         </Box>
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom>Title of your event: </Typography>
-                            <TextField fullWidth id="outlined-basic" label="Title" variant="outlined" />
+                            <TextField fullWidth id="outlined-basic" label="Title" variant="outlined" required value={title} onChange={(event) => setTitle(event.target.value)} />
                         </Box>
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom>Describe your event in 150 words or less: </Typography>
-                            <TextField fullWidth id="outlined-multiline-flexible" multiline maxRows={5} label="Description" variant="outlined" />
+                            <TextField fullWidth id="outlined-multiline-flexible" multiline maxRows={5} label="Description" variant="outlined" required value={description} onChange={(event) => setDescription(event.target.value)} />
                         </Box>
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom>Approximately how many attendees do you expect? </Typography>
-                            <TextField fullWidth id="outlined-basic" label="Attendees" variant="outlined" />
+                            <TextField fullWidth id="outlined-basic" label="Attendees" variant="outlined" required value={attendees} onChange={(event) => setAttendees(event.target.value)} />
                         </Box>
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom>Where will your event be hosted? </Typography>
-                            <FormControl component="fieldset">
+                            <FormControl component="fieldset" value={location} onChange={(event) => setLocation(event.target.value)}>
                                 <RadioGroup defaultValue="Online via the TCSW virtual venue" name="radio-buttons-group">
-                                    <FormControlLabel value="Online via the TCSW virtual venue" control={<Radio />} label="Online via the TCSW virtual venue" />
-                                    <FormControlLabel value="In Person" control={<Radio />} label="In-person" />
-                                    <FormControlLabel value="Other" control={<Radio />} label="Other" />
+                                    <FormControlLabel value={1} control={<Radio />} label="Online via the TCSW virtual venue" />
+                                    <FormControlLabel value={2} control={<Radio />} label="In-person" />
+                                    <FormControlLabel value={3} control={<Radio />} label="Other" />
                                 </RadioGroup>
                             </FormControl>
                         </Box>
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom>If your event is being held in-person, please share where you would like it to be hosted. </Typography>
                             <Typography variant="caption" display="block" gutterBottom> We would love to host community, culture, art, etc. events at our TCSW Community Hub or other cool spaces in the Cities.</Typography>
-                            <TextField fullWidth id="outlined-basic" label="Location" variant="outlined" />
+                            <TextField fullWidth id="outlined-basic" label="Location Details" variant="outlined" value={locationDetails} onChange={(event) => setLocationDetails(event.target.value)}/>
                         </Box>
                         <Box p={1}>
                             <Typography variant="body2" gutterBottom>What day(s) work for you to host your event? Select all that apply. </Typography>
