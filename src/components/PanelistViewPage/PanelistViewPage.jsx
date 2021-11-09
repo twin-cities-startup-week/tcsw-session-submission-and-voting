92e7bb905436535ac2a4e90eb5a6b3220bc2f312
Tou Xiong <tlxiong.prime@outlook.com>
2021-11-09 11:57:33 -0600

    added dummy text to show on dom

--- src/components/PanelistViewPage/PanelistViewPage.jsx
+++ src/components/PanelistViewPage/PanelistViewPage.jsx
@@ -1,8 +1,33 @@
+
+
 function PanelViewPage() {
 
 
     return(
-        <h1>Panel View Page</h1>
+        <div>
+            <h1>TCSW Panelists</h1>
+            <h5>Community Voting: date-date</h5>
+
+            <br/>
+
+            <h5>page</h5><h5>page</h5>
+            <table className='panelist-table'>
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Location</th>
+                    <th>Industry</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Rain green</td>
+                        <td>TCSW</td>
+                        <td>Technology</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
