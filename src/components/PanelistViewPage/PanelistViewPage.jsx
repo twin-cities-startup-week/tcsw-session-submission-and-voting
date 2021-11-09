05621c4998fd7dda50f1f7d195a0856972efb759
Tou Xiong <tlxiong.prime@outlook.com>
2021-11-09 14:11:03 -0600

    added the search feature layout to the panelist view page

--- src/components/PanelistViewPage/PanelistViewPage.jsx
+++ src/components/PanelistViewPage/PanelistViewPage.jsx
@@ -3,41 +3,84 @@ import { useHistory } from 'react-router-dom';
 function PanelViewPage() {
     const history = useHistory();
 
-    // const moveToSelectedPage = () => {
-    //     history.push()
-    // }
+    // navigates user to the details page of the speaker on click
+    const moveToSelectedPage = () => {
+        history.push('/user')
+    }
 
     return(
         <div>
-            <h1>TCSW Panelists</h1>
-            <h5>Community Voting: date-date</h5>
+            <div>
+                <h1>TCSW Panelists</h1>
+                <h5>Community Voting: date-date</h5>
 
-            <br/>
+                <br/>
 
-            <h5>page</h5>
+                <h5>page</h5>
 
-            <h5>
-                page
-                <button>back</button>
-                <button>forward</button>
-            </h5>
+                <h5>
+                    page
+                    <button>back</button>
+                    <button>forward</button>
+                </h5>
 
-            <table className='panelist-table'>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Location</th>
-                        <th>Industry</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Rain green</td>
-                        <td>TCSW</td>
-                        <td>Technology</td>
-                    </tr>
-                </tbody>
-            </table>
+                <table className='panelist-table'>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Location</th>
+                            <th>Industry</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td onClick={moveToSelectedPage}>Rain green</td>
+                            <td>TCSW</td>
+                            <td>Technology</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <h2>Search</h2>
+                <form>
+                    <input
+                        id='track-filter'
+                        type='text'
+                        name='track'
+                        placeholder='search'
+                    />
+
+                    <button>RESET</button>
+                    <button>SEARCH</button>
+
+                    <h5>Track</h5>
+                    <input
+                        id='track-filter'
+                        type='text'
+                        name='track'
+                        placeholder='track'
+                    />
+
+                    <h5>Format</h5>
+                    <input
+                        id='format-filter'
+                        type='text'
+                        name='format'
+                        placeholder='format'
+                    />
+                    
+                    <h5>Level</h5>
+                    <input
+                        id='level-filter'
+                        type='text'
+                        name='level'
+                        placeholder='level'
+                    />
+
+                </form>
+
+            </div>
         </div>
     )
 }
