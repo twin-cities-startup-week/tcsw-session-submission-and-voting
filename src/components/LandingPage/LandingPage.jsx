b8ad59f21429134b10a25c4c22f992ae740c69f4
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-11-09 13:27:47 -0600

    Removed already a member login button from landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -51,15 +51,6 @@ function LandingPage() {
             amet nisi.
           </p>
         </div>
-        <div className="grid-col grid-col_4">
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
       </div>
     </div>
   );
