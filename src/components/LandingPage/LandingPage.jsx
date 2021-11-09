05f64a15139a2abd5601200b5ef25338bb84eab1
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-11-09 13:28:30 -0600

    Removed grid spacing from landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -17,8 +17,8 @@ function LandingPage() {
     <div className="container">
       <h2>{heading}</h2>
 
-      <div className="grid">
-        <div className="grid-col grid-col_8">
+      <div className="">
+        <div>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
             id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
