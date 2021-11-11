8e13ede5c37b67b1fb111047d97bcc1144466d22
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-11-10 18:39:05 -0600

    added a faq page with it's questions

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 
+import { Paper, makeStyles } from "@material-ui/core";
+
+
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -17,34 +20,59 @@ function AboutPage() {
 
   
   return (
+    <Paper elevation={15}>
     <div className="container">
 
       <div>
       
-        <p>Twin Cities Startup Week is back from September 16 - 23, 2022!
+      <h2>TCSW Session Submission & Voting</h2>
+          <h4>2022 Session Submission Dates</h4>
+              <p> Session Submission Deadline: May 15, 2022</p>
+              <p> Session Voting Deadline: June 5, 2022</p>
 
-Interested in hosting a session? If you are an individual, business, or community organization doing amazing things in the world of startups and innovation, we'd love to work with you!
+        <p>Twin Cities Startup Week is back from September 16 - 23, 2022!</p>
 
-There are a few steps to organizing a TCSW session:
+<p>Interested in hosting a session? If you are an individual, business, or 
+  community organization doing amazing things in the world of startups 
+  and innovation, we'd love to work with you! </p>
 
-SESSION PROPOSALS ARE DUE MAY 1ST - NEED TO UPDATE
+<p>There are a few steps to organizing a TCSW session:</p>
 
-1. Use this form to submit your session proposal by May 1st. Once submitted, we will reach out with any questions that we have. We will notify you by ????? whether your sessions was accepted or not. Every year we receive more sessions that we have time slots on the schedule so if your session is not accepted, please think about applying next year!
+<p>SESSION PROPOSALS ARE DUE MAY 1ST - NEED TO UPDATE</p>
 
-2. If your session is accepted, final details are due ???. This will include speaker information, content summary, location, and timing/length. We know that things may change, but ask that you submit as much as possible so we can upload information into our master calendar prior to our schedule being announced on ????.
+<p>1. Use this form to submit your session proposal by May 1st. Once submitted, 
+  we will reach out with any questions that we have. We will notify you by ????? 
+  whether your sessions was accepted or not. Every year we receive more sessions 
+  that we have time slots on the schedule so if your session is not accepted, 
+  please think about applying next year! </p>
 
-3: Spread the word! We will work with you to help promote your session and make sure you - and your attendees - have an incredible experience. On ??? we will host an Event Host Webinar focused on how to prepare for and promote your TCSW session.
+<p>2. If your session is accepted, final details are due ???. 
+This will include speaker information, content summary, location, and timing/length. 
+We know that things may change, but ask that you submit as much as possible 
+so we can upload information into our master calendar prior to our schedule being announced on ????.</p>
 
-4: (Virtual sessions) Attend one of the Session Host TCSW Dress Rehearsals to test and practice using the technology and make sure you and your speakers are comfortable with the Hopin platform (taking place in August). 
+<p>3: Spread the word! We will work with you to help promote your session and make sure 
+  you - and your attendees - have an incredible experience. 
+  On ??? we will host an Event Host Webinar focused on how to prepare for and promote your TCSW session.</p>
 
-5: Attend the TCSW Event Host Happy Hour on ????. This will be a time you can learn more about other events happening throughout the week and connect with other event hosts.
+<p>4: (Virtual sessions) Attend one of the Session Host TCSW Dress Rehearsals to test 
+  and practice using the technology and make sure you and 
+  your speakers are comfortable with the Hopin platform (taking place in August). </p>
 
-6: Host your event! We're planning on having about 200 events this year.
+<p>5: Attend the TCSW Event Host Happy Hour on ????. 
+  This will be a time you can learn more about other events happening 
+  throughout the week and connect with other event hosts.</p>
 
-7: Provide feedback. It is important to us that event hosts, partners and attendees are given an opportunity to provide feedback. As an event host, we will send you a survey to gather feedback and capture any stories that came out of your events. We will also provide you a few questions to ask your attendees.
+<p>6: Host your event! We're planning on having about 200 events this year.</p>
+
+<p>7: Provide feedback. It is important to us that event hosts, 
+  partners and attendees are given an opportunity to provide feedback. 
+  As an event host, we will send you a survey to gather 
+  feedback and capture any stories that came out of your events. 
+  We will also provide you a few questions to ask your attendees.</p>
 
 
-If you have any questions or concerns about TCSW 2021, please don't hesitate to reach out to kelly@beta.mn.</p>
+<p>If you have any questions or concerns about TCSW 2021, please don't hesitate to reach out to kelly@beta.mn.</p>
 
 <button type="submit" onClick={learnMoreInfo}> Learn More </button>
 
@@ -55,6 +83,8 @@ If you have any questions or concerns about TCSW 2021, please don't hesitate to 
       dignissimos odio. Quasi ex nobis, tempora beatae ad hic alias?
       </p>}
     </div>
+
+    </Paper>
   );
 }
 
