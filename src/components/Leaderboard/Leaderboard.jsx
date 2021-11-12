4308911efae368628aeb5b52618603941b94ade6
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-11-12 11:18:22 -0600

    Added datagrid imports

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -3,6 +3,19 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@mui/styles';
+import { createTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import {
+  GridColumnMenu,
+  GridColumnMenuContainer,
+  GridFilterMenuItem,
+  SortGridMenuItems,
+  useGridApiRef,
+  DataGridPro,
+} from '@mui/x-data-grid-pro';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
 
 function Leaderboard() {
     return (
