e852d03c5efcbeb6c48bb596906957657c71dabf
Tou Xiong <tlxiong.prime@outlook.com>
2021-11-20 12:15:40 -0600

    able to filter specific track

--- src/components/PanelistViewPage/Panelists.jsx
+++ src/components/PanelistViewPage/Panelists.jsx
@@ -14,7 +14,8 @@ function Panelists() {
     console.log('this is the information from the panelistReducer', store.panelistReducer);
 
     const [ searchTerm, setSearchTerm ] = useState('');
-    const [ trackState, setTrackState ] = useState(''); 
+    const [ trackState, setTrackState ] = useState('');
+    const [ formatState, setFormatState ] =useState('');
 
     useEffect(() => {
             fetchPanelist();
@@ -35,89 +36,111 @@ function Panelists() {
     const votePageGo = () => {
         history.push('/votepage');
     }
+    
     return(
         <div>
             <h1>Panelist</h1>
 
-            {/* search bar with filtering on change */}
-            <input 
-                type='text'
-                placeholder='search....'
-                onChange={ event => { setSearchTerm( event.target.value )}}
-            />
-
-            
-            <div className='search-list' >
-            {/* <p onClick={ () => goToPanelDetails( speakers )}>{speakers.title}</p> */}
-
-            {/* <ul>
-                <li onClick={ () => goToPanelDetails( speakers )}>{speakers.title}</li>
-            </ul> */}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Title </th>
-                        <th>Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        {/* .map((speakers, key) => {
-                            return( */}
-
-                                    {store.panelistReducer.filter( panel  => panel.title.toLowerCase().includes(searchTerm.toLowerCase())).map( panel => (
-                                        // if( searchTerm == '') {
-                                        //     return panel;
-                                        // }else if( speakers.title.toLowerCase().includes(searchTerm.toLowerCase())) { 
-                                            <tr>
-                                                <td onClick={() => goToPanelDetails( panel )}>{panel.title}</td>
-                                                <td>{panel.location_details}</td>
-                                                <td>{panel.industry_id}</td>
-                                            </tr>
-                                            ) 
-                                    )}
-                                    
-                                        panel => panel.track = setTrackState .map
-                                        
-                              
-                                    //     {/* <td onClick={ () => goToPanelDetails( speakers )}>{speakers.title}</td>
-                                    //     <td>{speakers.location_details}</td>
-                                    // </tr> */}
-                            </tbody>
-                        </table>
-                    </div>
-                )
-                
-            {/* })} */}
-
-            {/* <form>
+            <form className='filter-buttons'>
                 <div>
-                    <h5>Track</h5>
+                    <input 
+                        type='text'
+                        placeholder='search....'
+                        onChange={ event => { setSearchTerm( event.target.value )}}
+                    />
+                    {/* <h5>Track</h5>
                         <button>Devloper</button>
                         <button>Designer</button>
-                        <button>Product</button>
+                        <button>Product</button> */}
+
                     <h5>Track</h5>
-                    <select className='track-selector'>
-                        <option value="growth"><p onClick={votePageGo}>growth</p></option>
-                        <option value="founder">founder</option>
-                        <option value="designer">designer</option>
+                    <select className='track-selector' onChange={ event => setTrackState( event.target.value )}>
+                        {console.log('this is the setTrackState',setTrackState)}
+                        {console.log('this is the trackState', trackState)}
+                        <option value=''> </option>
+                        <option value="Developer">Developer</option>
+                        <option value="Founder">Founder</option>
+                        <option value="Designer">Designer</option>
+                        <option value="Maker">Maker</option>
+                        <option value="Product">Product</option>
+                        <option value="Growth">Growth</option>
+                        <option value="People">People</option>
+                        <option value="Spotlight">Spotlight</option>
+                        <option value="Other">Other</option>
                     </select>
                 </div>
 
                 <div>
-                    <h5>Format</h5>
+                    {/* <h5>Format</h5>
                         <button>Presentation</button>
                         <button>Key Note</button>
-                        <button>Showcase</button>
+                        <button>Showcase</button> */}
+
                     <h5>Format</h5>
-                    <select>
-                        <option value="format1">format</option>
-                        <option value="format2">format</option>
-                        <option value="format3">format</option>
+                    <select onChange={ event => setFormatState( event.target.value )}>
+                        <option value=''> </option>
+                        <option value="PresentationPanel">PresentationPanel</option>
+                        <option value="Panel">Panel</option>
+                        <option value="Workshop">Workshop</option>
+                        <option value="Keynote">Keynote</option>
+                        <option value="Roundtable">Roundtable</option>
+                        <option value="Fireside Chat">Fireside Chat</option>
+                        <option value="Showcase">Showcase</option>
+                        <option value="Demo">Demo</option>
+                        <option value="Meetup">Meetup</option>
+                        <option value="Pitch">Pitch</option>
+                        <option value="Other">Other</option>
                     </select>
                 </div> 
             </form>
-                 */}
-                
+
+            
+            <div className='search-list' >
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Title </th>
+                            <th>Track</th>
+                            <th>Format</th>
+                        </tr>
+                    </thead>
+
+                    {trackState === '' && 
+                    <tbody>
+
+                        {store.panelistReducer.filter( panel  => panel.title.toLowerCase().includes(searchTerm.toLowerCase())).map( panel => (
+                                    
+                        <tr>
+                            <td onClick={() => goToPanelDetails( panel )}>{panel.title}</td>
+                            <td>{panel.track}</td>
+                            <td>{panel.format}</td>
+                        </tr>
+                                            
+                        ))}
+                    </tbody>
+                    }
+                    
+                    
+                    {trackState === 'Developer' &&
+                    <tbody>
+                        {store.panelistReducer.filter( panel  => panel.track.includes(trackState)).map( panel => (
+
+                        <tr>
+                            <td onClick={() => goToPanelDetails( panel )}>{panel.title}</td>
+                            <td>{panel.track}</td>
+                            <td>{panel.format}</td>
+                        </tr>
+
+                        ))}
+
+
+                                    
+                            {/* panel => panel.track = setTrackState .map */}
+    
+                    </tbody>
+                    }
+                </table>
+            </div>       
 
         </div>
     )
