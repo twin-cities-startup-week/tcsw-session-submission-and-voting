10da52360eb7fd1acb6f5e88ae1d5e823374708a
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-11-27 13:17:17 -0600

    Added login form code comments

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -21,6 +21,7 @@ function LoginForm() {
   const login = (event) => {
     event.preventDefault();
 
+    /* If the username and password match, then log in that user */
     if (username && password) {
       dispatch({
         type: 'LOGIN',
@@ -30,6 +31,8 @@ function LoginForm() {
         },
       });
     } else {
+      /* If the username and password do not match, display the appropriate error
+          from the redux store. These error are in the errors reducer */
       dispatch({ type: 'LOGIN_INPUT_ERROR' });
     }
   }; // end login
@@ -46,7 +49,10 @@ function LoginForm() {
 
       <h2 className="registerHeader">Sign In</h2>
 
-      {/* <GoogleSignin/> */}
+      {/* GoogleSignin, 
+          code related to google Oauth is in server.js 
+          This uses the user's gmail as the username, and displays their
+          first name provided through gmail in the upper right hand corner */}
       <Box sx={{ textAlign: 'center', mb: 2 }}>
       <GoogleLogin>
         <Button 
@@ -59,11 +65,16 @@ function LoginForm() {
       </Box>
 
       <form onSubmit={login}>
+
+        {/* When an error occurs with login, the matching message will appear 
+          on the login form */}
         {errors.loginMessage && (
           <h3 className="alert" role="alert">
             {errors.loginMessage}
           </h3>
         )}
+
+        {/* Login form, username input */}
         <Box sx={{ textAlign: 'center' }}>
           <Box sx={{ m: 1 }}>
             <label htmlFor="username">
@@ -75,6 +86,8 @@ function LoginForm() {
               />
             </label>
           </Box>
+
+          {/* Login form, password input */}
           <Box sx={{ m: 1 }}>
             <label htmlFor="password">
               <TextField sx={{ width: 500, bgcolor: '#FFFFFF', borderRadius: 1, mb: 1 }}
@@ -85,6 +98,8 @@ function LoginForm() {
               />
             </label>
           </Box>
+
+          {/* Forgot your password link */}
           <center>
             <button type="button" className="btn btn_asLink"
               onClick={() => {
@@ -93,19 +108,24 @@ function LoginForm() {
                 Forgot your password?
             </button>
           </center>
+
+          {/* Sign in button  */}
           <Button variant="contained" type="submit" name="submit" value="Log In"
           sx={{ mt: 2, mb: 2, p: 2, width: 350, height: 50, bgcolor: '#0C495A', color: '#FBBD19' }}
             > Sign In</Button>
         </Box>
       </form>
-
+      
+      {/* Yellow container for "Need to sign up?" */}
       <Box component={Paper} elevation={6} 
         sx={{ bgcolor: '#FBBD19', borderRadius: 1, width: 500, height: 55, m: 'auto' }}>
-
+        
+        {/* Need to sign up text inside yellow container */}
         <Typography 
           sx={{ display: 'inline-block', textAlign: 'center', mt: 2, ml: 10 }}
           >Need to Sign up?</Typography>
 
+        {/* Create Account Button */}
         <Button component={Paper} elevation={8} 
           variant="contained" type="submit" name="submit" value="Log In"
           sx={{ mt: 1, p: 2, width: 200, height: 40, bgcolor: '#0C495A', 
