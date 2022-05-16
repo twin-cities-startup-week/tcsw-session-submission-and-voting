495a77c396a2288c3ae9dd90695dba85db272051
Christopher Black <chris@blacktopinteractive.com>
2022-05-16 11:07:19 -0500

    Updated error text for login.

--- src/redux/reducers/errors.reducer.js
+++ src/redux/reducers/errors.reducer.js
@@ -11,7 +11,7 @@ const loginMessage = (state = '', action) => {
     case 'LOGIN_FAILED':
       return "Oops! The username and password didn't match. Try again!";
     case 'LOGIN_FAILED_NO_CODE':
-      return 'Oops! Something went wrong! Is the server running?';
+      return 'Unable to find an account matching that username and password. If the problem persists, please reach out to tcsw@beta.mn so that we can help.';
     case 'PASSWORD_RESET_FAILED':
       return "Oops! The username and email didn't match. Try again!";
     default:
